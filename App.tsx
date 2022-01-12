dd8e7cb709b6cac086cb5c21a0b77342763430d3
Charles Pereira <charlesti.ptu@gmail.com>
2022-01-11 23:45:50 -0300

    refecture

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
 
-import SignIn from '@screens/SignIn';
+import SignIn from './src/screens/SignIn';
 import theme from './src/theme';
 
 export const App: React.FC = () => {
