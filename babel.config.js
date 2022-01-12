e647dbb62d963d742b1b4a225603b010cd574641
Charles Pereira <charlesti.ptu@gmail.com>
2022-01-11 23:46:02 -0300

    refecture

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin",
+      // 'react-native-reanimated/plugin',
       [
-        "module-resolver",
+        'module-resolver',
         {
-          root: ["./src"],
-          extensions: [".ts", ".tsx", ".js", ".json"],
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.js', '.json'],
           alias: {
-            "@components": "./src/components",
-            "@screens": "./src/screens",
-            "@assets": "./src/assets",
+            'fastfood/': './src/',
+            // "@components": "./src/components",
+            // "@screens": "./src/screens",
+            // "@assets": "./src/assets",
           },
         },
       ],
