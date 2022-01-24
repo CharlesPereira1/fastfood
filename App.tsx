11c7d497bae71944473ddbeeb4b9ed49c42d22d4
Charles Pereira <charlesti.ptu@gmail.com>
2022-01-24 00:22:23 -0300

    BugFix: problem resolver expo app loading with instalation expo install expo-modules-core

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {} from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
 import SignIn from '@fastfood/screens/SignIn';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <SignIn />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
