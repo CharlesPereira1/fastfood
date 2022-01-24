6d2b5ce674a23f03ff5be9eeb8cd4546e09a7457
Charles Pereira <charlesti.ptu@gmail.com>
2022-01-24 00:52:08 -0300

    Component: input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { Input } from '@fastfood/components/Input';
+
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Text>Sigin</Text>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
     </Container>
   );
 };
